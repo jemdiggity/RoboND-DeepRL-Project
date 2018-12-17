63afc527323eefab83642652af45f4e549064b81
Jeremy Hale <jeremy.hale@gmail.com>
2018-12-16 18:03:17 -0800

    3 points for going wrong way, 1 point just for being alive

--- gazebo/ArmPlugin.cpp
+++ gazebo/ArmPlugin.cpp
@@ -651,9 +651,11 @@ void ArmPlugin::OnUpdate(const common::UpdateInfo& updateInfo)
 				// compute the smoothed moving average of the delta of the distance to the goal
 				// avgGoalDelta  = (avgGoalDelta * 0.5) + (distDelta * 0.5);
 				// avgGoalDelta = distDelta;
+				newReward = true;
 				if (distDelta > 0) {
+					rewardHistory += 3*REWARD_LOSS;
+				} else {
 					rewardHistory += REWARD_LOSS;
-					newReward = true;
 				}
 			}
 
