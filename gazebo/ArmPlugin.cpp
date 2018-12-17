a646c4a42ceb4b170d5f44646a6313a0bcae5adc
Jeremy Hale <jeremy.hale@gmail.com>
2018-12-16 16:49:13 -0800

    Reduce ground contact distance

--- gazebo/ArmPlugin.cpp
+++ gazebo/ArmPlugin.cpp
@@ -608,7 +608,7 @@ void ArmPlugin::OnUpdate(const common::UpdateInfo& updateInfo)
 
 		// get the bounding box for the gripper
 		const math::Box& gripBBox = gripper->GetBoundingBox();
-		const float groundContact = 0.05f;
+		const float groundContact = 0.01f;
 
 		/*
 		/ TODO - set appropriate Reward for robot hitting the ground.
