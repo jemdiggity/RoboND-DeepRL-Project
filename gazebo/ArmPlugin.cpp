f497c124dc9fb6d4bba834e8b70df674142946ea
Jeremy Hale <jeremy.hale@gmail.com>
2018-12-17 20:38:10 -0800

    Adjust reward function and hyperparams batch_size and lstm_size

--- gazebo/ArmPlugin.cpp
+++ gazebo/ArmPlugin.cpp
@@ -40,9 +40,9 @@
 #define OPTIMIZER "RMSprop"
 #define LEARNING_RATE 0.001f
 #define REPLAY_MEMORY 10000
-#define BATCH_SIZE 256
+#define BATCH_SIZE 64
 #define USE_LSTM true
-#define LSTM_SIZE 256
+#define LSTM_SIZE 64
 
 /*
 / TODO - Define Reward Parameters
@@ -308,7 +308,7 @@ void ArmPlugin::onCollisionMsg(ConstContactsPtr &contacts)
 
 		if (collisionCheck)
 		{
-			rewardHistory = 100 * REWARD_WIN;
+			rewardHistory = REWARD_WIN;
 
 			newReward  = true;
 			endEpisode = true;
@@ -627,7 +627,7 @@ void ArmPlugin::OnUpdate(const common::UpdateInfo& updateInfo)
 
 			if(true){printf("GROUND CONTACT, EOE\n");}
 			if (!endEpisode) {
-				rewardHistory += 10 * REWARD_LOSS;
+				rewardHistory = REWARD_LOSS;
 			}
 			newReward     = true;
 			endEpisode    = true;
