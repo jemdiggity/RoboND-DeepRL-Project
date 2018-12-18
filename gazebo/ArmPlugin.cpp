1c173ffc7061cc513b60d31debb165817386691d
Jeremy Hale <jeremy.hale@gmail.com>
2018-12-17 21:07:30 -0800

    Set batch and lstm size to 256

--- gazebo/ArmPlugin.cpp
+++ gazebo/ArmPlugin.cpp
@@ -40,9 +40,9 @@
 #define OPTIMIZER "RMSprop"
 #define LEARNING_RATE 0.001f
 #define REPLAY_MEMORY 10000
-#define BATCH_SIZE 64
+#define BATCH_SIZE 256
 #define USE_LSTM true
-#define LSTM_SIZE 64
+#define LSTM_SIZE 256
 
 /*
 / TODO - Define Reward Parameters
